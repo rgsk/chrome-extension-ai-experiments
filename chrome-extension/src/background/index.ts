6937f8ec0db7bbf8d18550cbe1bfe1d072d86e3f
rgsk <guptarahul70322@gmail.com>
2025-12-16 22:18:24 +0530

    base setup

---
+++ chrome-extension/src/background/index.ts
@@ -0,0 +1,19 @@
+import { exampleThemeStorage } from "@extension/storage";
+import "webextension-polyfill";
+
+exampleThemeStorage.get().then((theme) => {
+  console.log("theme", theme);
+});
+
+console.log("Background loaded");
+console.log(
+  "Edit 'chrome-extension/src/background/index.ts' and save to reload.",
+);
+
+chrome.commands.onCommand.addListener(() => {
+  chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
+    if (tab.id) {
+      chrome.sidePanel.open({ tabId: tab.id });
+    }
+  });
+});
