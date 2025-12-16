2e8a79f4e13cbb4b74c969462b5641edc2691139
rgsk <guptarahul70322@gmail.com>
2025-12-16 23:43:27 +0530

    fix

--- pages/side-panel/src/SidePanel.tsx
+++ pages/side-panel/src/SidePanel.tsx
@@ -1,46 +1,21 @@
-import { t } from "@extension/i18n";
-import {
-  PROJECT_URL_OBJECT,
-  useStorage,
-  withErrorBoundary,
-  withSuspense,
-} from "@extension/shared";
-import { exampleThemeStorage } from "@extension/storage";
-import { cn, ErrorDisplay, LoadingSpinner, ToggleButton } from "@extension/ui";
+import { withErrorBoundary, withSuspense } from "@extension/shared";
+import { ErrorDisplay, LoadingSpinner } from "@extension/ui";
 import "@src/SidePanel.css";
+import { useMemo } from "react";
+import { v4 } from "uuid";
 
 chrome.runtime.connect({ name: "mySidepanel" });
 
 const SidePanel = () => {
-  const { isLight } = useStorage(exampleThemeStorage);
-  const logo = isLight
-    ? "side-panel/logo_vertical.svg"
-    : "side-panel/logo_vertical_dark.svg";
-
-  const goGithubSite = () => chrome.tabs.create(PROJECT_URL_OBJECT);
+  const chatId = useMemo(() => v4(), []);
 
   return (
-    <div className={cn("App", isLight ? "bg-slate-50" : "bg-gray-800")}>
-      <header
-        className={cn(
-          "App-header",
-          isLight ? "text-gray-900" : "text-gray-100",
-        )}
-      >
-        <button onClick={goGithubSite}>
-          <img
-            src={chrome.runtime.getURL(logo)}
-            className="App-logo"
-            alt="logo"
-          />
-        </button>
-        <p>
-          Edit <code>pages/side-panel/src/SidePanel.tsx</code>
-        </p>
-        <ToggleButton onClick={exampleThemeStorage.toggle}>
-          {t("toggleTheme")}
-        </ToggleButton>
-      </header>
+    <div className="h-screen">
+      <iframe
+        title="React AI Experiments"
+        src={`http://localhost:5173/chat/${chatId}`}
+        className="w-full h-full"
+      ></iframe>
     </div>
   );
 };
