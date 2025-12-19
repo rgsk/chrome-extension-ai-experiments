579006f784323f96b1ca8d023a28eecd5190cef1
rgsk <guptarahul70322@gmail.com>
2025-12-19 13:41:20 +0530

    fix

--- pages/content/src/matches/all/index.ts
+++ pages/content/src/matches/all/index.ts
@@ -1,5 +1,21 @@
-import { sampleFunction } from '@src/sample-function';
+import { sampleFunction } from "@src/sample-function";
 
-console.log('[CEB] All content script loaded');
+console.log("[CEB] All content script loaded");
 
 void sampleFunction();
+
+chrome.runtime.onMessage.addListener((msg, _, sendResponse) => {
+  if (msg.type === "PAGE_DIMENSIONS") {
+    sendResponse({
+      width: document.documentElement.scrollWidth,
+      height: document.documentElement.scrollHeight,
+      viewportHeight: window.innerHeight,
+    });
+  }
+
+  if (msg.type === "SCROLL_TO") {
+    window.scrollTo(0, msg.y);
+    setTimeout(sendResponse, 300);
+    return true;
+  }
+});
